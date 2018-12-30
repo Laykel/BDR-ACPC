59a0d06b835cf67c937afc0aaf2d5ca865de5313
Mateo Tutic <mateo@tutic.ch>
2018-12-30 15:53:55 +0100

    Filtre - Collapsible panel sans formulaire

--- sources/pages/home.php
+++ sources/pages/home.php
@@ -3,8 +3,43 @@ Filename: home.php
 Description: Projects page: Projects and tasks, timers, update details.
 -->
 
+<div class="wrapper center-block">
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="panel panel-default">
+      <div class="panel-heading active" role="tab" id="headingOne">
+        <h4 class="panel-title">
+          <a role="button"
+             data-toggle="collapse"
+             data-parent="#accordion"
+             href="#collapseFiltre"
+             aria-expanded="true"
+             aria-controls="collaps">
+            Filtres
+          </a>
+        </h4>
+      </div>
+      <div id="collapseFiltre" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+        <div class="panel-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="row">
   <h1>Holà mundo</h1>
   <?php var_dump($test->fetchAll()); ?>
 </div>
+
+<script>
+  $(function() {
+    $('.panel-collapse').on('show.bs.collapse', function () {
+      $(this).siblings('.panel-heading').addClass('active');
+    });
+
+    $('.panel-collapse').on('hide.bs.collapse', function () {
+      $(this).siblings('.panel-heading').removeClass('active');
+    });
+  });
+</script>
