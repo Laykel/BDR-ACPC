1fa90edc8c2d6a40d63ed77c28456e77cd4f66b7
Mateo Tutic <mateo@tutic.ch>
2018-12-30 19:45:41 +0100

    Filtre - Panel collapsible par défaut ouvert

--- sources/pages/home.php
+++ sources/pages/home.php
@@ -13,12 +13,12 @@ Description: Projects page: Projects and tasks, timers, update details.
              data-parent="#accordion"
              href="#collapseFiltre"
              aria-expanded="true"
-             aria-controls="collaps">
+             aria-controls="collapseFiltre">
             Filtres
           </a>
         </h4>
       </div>
-      <div id="collapseFiltre" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div id="collapseFiltre" class="panel-collapse collapse in show" role="tabpanel">
         <div class="panel-body">
           Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
         </div>
